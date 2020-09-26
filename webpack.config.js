8a88f19e483df5ed2f671158e5bdac2936c7f909
James Hill <me@jhdev.uk>
2020-09-26 22:54:38 +0100

    Changed webpack config to multi-compiler

--- webpack.config.js
+++ webpack.config.js
@@ -34,13 +34,19 @@ const sassLoader = {
 
 /* -----------------------------------
  *
- * Config
+ * Pages
  *
  * -------------------------------- */
 
-module.exports = {
+const pages = {
   mode: 'development',
-  entry: glob.sync(__dirname + '/src/**/*.11ty.ts*').reduce(getEntryFile, {}),
+  entry: glob.sync(`${__dirname}/src/**/*.11ty.ts*`).reduce((result, file) => {
+    const [name] = file.split('src/').slice(-1);
+
+    result[name.replace('.tsx', '')] = file;
+
+    return result;
+  }, {}),
   context: path.join(__dirname, '/src/'),
   cache: true,
   target: 'node',
@@ -146,10 +152,116 @@ module.exports = {
  *
  * -------------------------------- */
 
-function getEntryFile(result, file) {
-  const [name] = file.split('src/').slice(-1);
+const entry = {
+  mode: RELEASE ? 'production' : 'development',
+  entry: glob.sync(`${__dirname}/src/entry/*.entry.ts*`).reduce((result, file) => {
+    const name = path.basename(file, path.extname(file));
 
-  result[name.replace('.tsx', '')] = file;
+    result[name] = file;
 
-  return result;
-}
+    return result;
+  }, {}),
+  context: path.join(__dirname, '/src/'),
+  cache: true,
+  target: 'web',
+  output: {
+    path: path.join(__dirname, '/src/_js/assets'),
+    filename: RELEASE ? '[name].[chunkhash:8].js' : '[name].js',
+    publicPath: '/',
+  },
+  resolve: {
+    extensions: ['.js', '.ts', '.tsx', '.json', '.scss'],
+    alias: {
+      '@': path.resolve(__dirname, `./src/`),
+    },
+  },
+  plugins: [
+    new AssetsManifestPlugin({
+      output: '../assets.json',
+      merge: true,
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: [
+          {
+            loader: 'ts-loader',
+            options: {
+              compilerOptions: {
+                target: 'es5',
+              },
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(sa|sc|c)ss$/,
+        exclude: /\.module\.(sa|sc|c)ss$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              modules: 'global',
+              importLoaders: 2,
+              sourceMap: false,
+            },
+          },
+          sassLoader,
+        ],
+      },
+      {
+        test: /\.module\.(sa|sc|c)ss$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              modules: {
+                localIdentName: '[local]_[hash:base64:6]',
+              },
+              importLoaders: 2,
+              sourceMap: false,
+            },
+          },
+          sassLoader,
+        ],
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[hash:8].[ext]',
+            },
+          },
+        ],
+      },
+    ],
+  },
+  optimization: {
+    moduleIds: 'hashed',
+    runtimeChunk: 'single',
+    splitChunks: {
+      cacheGroups: {
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          name: 'vendor',
+          chunks: 'all',
+          enforce: true,
+        },
+      },
+    },
+  },
+};
+
+/* -----------------------------------
+ *
+ * Export
+ *
+ * -------------------------------- */
+
+module.exports = [pages, entry];
