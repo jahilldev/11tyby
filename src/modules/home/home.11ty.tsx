cd1c85b797946940fb0e040e974d367b57db6744
James Hill <jahill@groupon.com>
2021-01-05 16:48:44 +0000

    Reverted home page title

--- src/modules/home/home.11ty.tsx
+++ src/modules/home/home.11ty.tsx
@@ -28,7 +28,7 @@ function Page(this: IPage) {
       jsPath="home/home.entry.js"
     >
       <main class={style.content}>
-        <p class={style.text}>11tyboo Demo</p>
+        <p class={style.text}>11tyboo</p>
         <a href="/articles/first-post" class={style.link}>
           Go to the First post
         </a>
