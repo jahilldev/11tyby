e3db2d9de74a6208eed8254ad2df5d8421a3378f
James Hill <jahill@groupon.com>
2021-02-16 16:55:33 +0000

    Updated packages and fixed entry file chunks

--- webpack.config.js
+++ webpack.config.js
@@ -259,6 +259,7 @@ const entry = {
             options: {
               compilerOptions: {
                 target: 'es5',
+                module: 'esnext',
               },
             },
           },
